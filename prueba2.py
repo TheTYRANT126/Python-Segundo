d108fb9b340b77ddd0bfc5ca44e3b05b79190495
TheTYRANT126 <spidermanvenom1206@hotmail.com>
2025-09-09 13:50:22 -0600

    nuevo cambio

--- prueba2.py
+++ prueba2.py
@@ -1,7 +1,11 @@
 
 print("este es un nuevo mensaje 2,0 xd");
 
-print("este es un nuevo mensaje jeje xd loquendo");
+print("este es un nuevo mensaje jeje xd loquendogitr");
+
+
+print("Esto es nuevo en el main");
+
 
 
 
